ab92d1eb6007ad6b70f7a2017487c73cfd47fcb1
Maurizio Brioschi <maurizio.brioschi@gmail.com>
2020-04-15 18:34:08 +0200

    traefikv2.2 set up

---
+++ reverseProxy-start1.7.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "create network reverse-proxy"
+docker network create -d bridge reverse-proxy
+
+echo "== start traefik v1.7"
+docker run -d \
+    -p 80:80 \
+    -p 8080:8080 \
+    -v $(pwd)/configuration/traefik/traefik1.7.toml:/etc/traefik/traefik.toml \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -l traefik.frontend.rule=Host:monitor.docker.local \
+    -l traefik.port=8080 \
+    --name traefik \
+    --network reverse-proxy \
+    traefik:v1.7
--- reverseProxy-start2.2.sh
+++ reverseProxy-start2.2.sh
@@ -2,14 +2,15 @@
 echo "create network reverse-proxy"
 docker network create -d bridge reverse-proxy
 
-echo "== start traefik"
+echo "== start traefik v2.2"
 docker run -d \
     -p 80:80 \
     -p 8080:8080 \
-    -v $(pwd)/configuration/traefik/traefik1.7.toml:/etc/traefik/traefik.toml \
+    -p 3306:3306 \
+    -v $(pwd)/configuration/traefik/traefik2.2.toml:/etc/traefik/traefik.toml \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -l traefik.frontend.rule=Host:monitor.docker.local \
     -l traefik.port=8080 \
     --name traefik \
     --network reverse-proxy \
-    traefik:v1.7
+    traefik:v2.2
